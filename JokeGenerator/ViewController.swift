cd35b4ce3ce259bddaccf070393fbc350ff20024
Morgan Duverney <morgan.duverney@gmail.com>
2021-06-01 11:16:49 -0400

    Initial Commit

---
+++ JokeGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JokeGenerator
+//
+//  Created by Morgan Duverney on 6/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
